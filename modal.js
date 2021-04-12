7d1e0b32480f0a499d215b0927be24074c3b8d54
YaroslavBorshevsky812 <Speransky812@mail.ru>
2021-04-12 21:49:00 +0300

    modalBtns added

--- modal.js
+++ modal.js
@@ -1,6 +1,25 @@
-const modalBtn = document.querySelector('.modal_button')
+const modalMainBtn = document.querySelector('.modal_button')
 const modal = document.querySelector('.modal')
+const modalBtn = document.querySelectorAll('[data-button]')
+const modalDelBtn = document.querySelector('[data-del]')
 
-modalBtn.addEventListener('click', () => {
-    modal.classList.add('active')
+
+modalMainBtn.addEventListener('click', () => {
+    modal.classList.toggle('active')
 })
+
+modalBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(aswr.value === '.' && aswr.value.includes('.')) return
+        if((aswr.value.length + 1) > 3) {
+            aswr.value = aswr.value.toString()
+        } else {
+            aswr.value = aswr.value.toString() + btn.innerText.toString()
+        }
+    })
+})
+
+modalDelBtn.addEventListener('click', () => {
+    aswr.value = aswr.value.toString().slice(0, -1)
+})
+
